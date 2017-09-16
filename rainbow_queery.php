49481bc86e991eeb2b388ba83eec25b35138183f
jolarti <jolarti@gmail.com>
2017-09-16 18:19:50 +0200

    Version 1.1
    Description: add true rainbow styles and fabulous queer hover state

--- rainbow_queery.php
+++ rainbow_queery.php
@@ -3,7 +3,7 @@
 Plugin Name: Rainbow Queery
 Plugin URI: https://github.com/jolarti/LGBT-quotes-wordpress-plugin
 Description: A queer plugin spin-off of the Hello Dolly plug-in that displays inspirational, humorous or activist quotes for people of all LGBTQ rainbow diversities at the top of your admin screen.
-Version: 1.0
+Version: 1.1
 Author: Jiro Ghianni
 Author URI: http://www.jirosworld.com/
 License: GPLv2
@@ -20,6 +20,7 @@ If homosexuality is a disease, let's call in queer to work. 'Hello, can't work t
 'Being queer is like being on a lifetime assignment as a secret agent in some foreign country.'' -Noretta Koertge
 'Women who seek to be equal with men lack ambition.' -Marilyn Monroe
 'Be yourself; everyone else is already taken.' -Oscar Wilde
+'If you are against gay marriage then just don't marry a gay dude.' -Bianca del Rio
 'You only live once, but if you do it right, once is enough.' -Mae West
 Why do people say 'grow some balls'? Balls are weak and sensitive. If you wanna be tough, grow a vagina. Those things can take a pounding! -Betty White
 'Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.' -Dr. Seuss
@@ -40,7 +41,7 @@ My mother said to me, 'Why do you have to call yourself a dyke? Why can't you be
 // Echoes the chosen line, we'll position it later
 function say_queery() {
 	$chosen = rainbow_queery_get_quote();
-	echo "<p id='LGBTQ'>$chosen</p>";
+	echo "<p id='LGBTQ'><a href=''>$chosen</a></p>";
 }
 
 // Set that function up to execute when the admin_notices action is called
@@ -53,17 +54,76 @@ function lgbtq_css() {
 
 	echo "
 	<style type='text/css'>
-	#LGBTQ {
-		float: $x;
-		padding: 8px;		
-		margin: 0;
-		font-size: 12px;
-		background-color: #ffccff;
-		border-radius: 1em;
-		box-shadow: 1px 1px 8px 7px pink inset;
-		-moz-box-shadow: 1px 1px 8px 7px pink inset;
-		-webkit-box-shadow: 1px 1px 8px 7px pink inset;
-	}
+p#LGBTQ>a {
+	color: #000000;
+	text-decoration: none;
+}
+#LGBTQ {
+	float: right;
+    padding: 10px;
+    margin: 0 1em 0 0;
+    max-width: 30%;
+    line-height: 1.7;
+	font-size: 12px;
+	background-color: #f3b4f3;
+	border-radius: 1em;
+	z-index: auto;
+	cursor: pointer;
+	opacity: 1;
+	-webkit-border-radius: 1em;
+	border-radius: 1em;
+	-webkit-box-shadow: 3px 0 0 rgb(217, 31, 38),
+	6px 0 0 rgb(226, 91, 14),
+	9px 0 0 rgb(245, 221, 8),
+	12px 0 0 rgb(5, 148, 68),
+	15px 0 0 rgb(2, 135, 206),
+	18px 0 0 rgb(4, 77, 145),
+	21px 0 0 rgb(42, 21, 113);
+	box-shadow: 3px 0 0 rgb(217, 31, 38),
+	6px 0 0 rgb(226, 91, 14),
+	9px 0 0 rgb(245, 221, 8),
+	12px 0 0 rgb(5, 148, 68),
+	15px 0 0 rgb(2, 135, 206),
+	18px 0 0 rgb(4, 77, 145),
+	21px 0 0 rgb(42, 21, 113);
+	-webkit-transition: all 600ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	-moz-transition: all 600ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	-o-transition: all 600ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	transition: all 600ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	-webkit-transform: none;
+	transform: none;
+	-webkit-transform-origin: 50% 50% 0;
+	transform-origin: 50% 50% 0;
+}
+#LGBTQ:hover, #LGBTQ:focus {
+	z-index: auto;
+	cursor: pointer;
+	opacity: 1;
+	-webkit-border-radius: 1em;
+	border-radius: 1em;
+	-webkit-box-shadow: -3px 0 0 rgb(217, 31, 38),
+	-6px 0 0 rgb(226, 91, 14),
+	-9px 0 0 rgb(245, 221, 8),
+	-12px 0 0 rgb(5, 148, 68),
+	-15px 0 0 rgb(2, 135, 206),
+	-18px 0 0 rgb(4, 77, 145),
+	-21px 0 0 rgb(42, 21, 113);
+	box-shadow: -3px 0 0 rgb(217, 31, 38),
+	-6px 0 0 rgb(226, 91, 14),
+	-9px 0 0 rgb(245, 221, 8),
+	-12px 0 0 rgb(5, 148, 68),
+	-15px 0 0 rgb(2, 135, 206),
+	-18px 0 0 rgb(4, 77, 145),
+	-21px 0 0 rgb(42, 21, 113);
+	-webkit-transition: all 500ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	-moz-transition: all 500ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	-o-transition: all 500ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	transition: all 500ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+	-webkit-transform: none;
+	transform: none;
+	-webkit-transform-origin: 50% 50% 0;
+	transform-origin: 50% 50% 0;
+}
 	</style>
 	";
 }
